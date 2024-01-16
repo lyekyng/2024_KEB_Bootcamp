d0fba3f0b2e8f24de6dd97c29286e9c84c1b1080
lyekyng <lyekyng@gmail.com>
2024-01-16 16:32:56 +0900

    240116_task2_02

--- day03.py
+++ day03.py
@@ -1,5 +1,14 @@
-#연습문제 6.5_1
+#연습문제 6.5_2
+guess_me = 7
+number = 1
+while (True):
+    if number < guess_me:
+        print("too low")
+    elif number == guess_me:
+        print ("found it!")
+        break
+    else:
+        print("oops")
+        break
+    number = number +1
 
-list = [3, 2, 1, 0]
-for i in list:
-    print (i)
