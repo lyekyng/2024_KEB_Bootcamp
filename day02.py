5fb1c48cb7c7fac03a8804bb0dc82b0b1d8f3547
lyekyng <lyekyng@gmail.com>
2024-01-16 10:03:49 +0900

    new

--- day02.py
+++ day02.py
@@ -1,12 +1,5 @@
-#2024_01_15_PythonProject_day2
-while True:
-    manu = input("1)Fahrenheit -> Celsius   2) Celsius -> Fahrenheit   3) Quit program :")
-    if manu == '3':
-        print("End Celsius conversion program")
-        break
-    if manu == '1':
-        fahrenheit = float(input('Input Fahrenheit : '))
-        print(f'Fahrenheit : {fahrenheit}F, Celsius : {((fahrenheit - 32.0) * 5.0 / 9.0):.4f}C')
-    elif manu == '2':
-        celsius = float(input('Input Celcius : '))
-        print(f'Celsius : {celsius}C, Fahrenheit : {((celsius * 9.0 / 5.0) + 32.0):.4f}F')
+uni = "inha \n  university"
+print(uni)
+num1 = input("First num: ")
+num2 = input("second num: ")
+print (num1 + num2)
