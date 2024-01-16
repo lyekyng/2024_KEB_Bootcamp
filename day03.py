f7a405b0c16387773e212a0dc01a4b48f5fbc9e3
lyekyng <lyekyng@gmail.com>
2024-01-16 16:34:11 +0900

    240116_task2_03

--- day03.py
+++ day03.py
@@ -1,14 +1,12 @@
-#연습문제 6.5_2
-guess_me = 7
-number = 1
-while (True):
+#연습문제 6.5_03
+guess_me = 5
+for number in range(10):
     if number < guess_me:
         print("too low")
-    elif number == guess_me:
-        print ("found it!")
+    elif number == guess_me :
+        print("found it !")
         break
     else:
-        print("oops")
+        print ("oops")
         break
-    number = number +1
 
