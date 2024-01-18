d1bbc84618daa8ea5ae2481a6dfc4698bc01609a
lyekyng <lyekyng@gmail.com>
2024-01-18 18:15:45 +0900

    day04_8.3_2

--- day04.py
+++ day04.py
@@ -1,4 +1,4 @@
 #연습문제 3
 e2f = dict(dog = "chien", cat = "chat", walrus = "morse")
-fe2 = dict((value, key) for key, value in e2f.items())
-print(e2f)
+f2e = dict((value, key) for key, value in e2f.items())
+print(f2e)
