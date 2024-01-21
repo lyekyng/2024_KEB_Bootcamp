2134ac598ce4f202678796e76f44d203e7a1182a
lyekyng <lyekyng@gmail.com>
2024-01-21 17:31:09 +0900

    is_palindrome

--- day05.py
+++ day05.py
@@ -1,3 +1,20 @@
-#연습문제 1
-e2f = dict(dog = "chien", cat = "chat", walrus = "morse")
+# 사용자로부터 문자열을 입력받는 코드를 작성하세요.
+
+# 입력받은 문자열이 팰린드롬인지 여부를 판별하는 함수를 작성하세요.
+
+# 판별 결과를 출력하는 코드를 작성하세요.
+
+def is_P(str):
+    str = str.lower()
+    str2 = str[-1::-1]
+    if str == str2 :
+        return True
+    else :
+        return False
+
+while True :
+    str = input("input String ... ")
+    print(is_P(str))
+
+
 
